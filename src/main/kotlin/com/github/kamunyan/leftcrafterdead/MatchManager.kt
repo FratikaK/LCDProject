a8c0b22b7cdca70545b57ee5444095c2bc5bbd50
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-07-28 22:56:51 +0900

    [add]ゲームプレイヤーの処理を追加

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/MatchManager.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/MatchManager.kt
@@ -1,4 +1,21 @@
 package com.github.kamunyan.leftcrafterdead
 
+import com.github.kamunyan.leftcrafterdead.player.L4DPlayer
+import org.bukkit.entity.Player
+import org.jetbrains.annotations.NotNull
+
 object MatchManager {
+
+    val onlineL4DPlayer = HashMap<String, L4DPlayer>()
+    val matchPlayer = mutableListOf<L4DPlayer>()
+
+    @NotNull
+    @Synchronized
+    fun getL4DPlayer(@NotNull target: Player): L4DPlayer {
+        val uuid = target.uniqueId.toString()
+        if (!onlineL4DPlayer.containsKey(uuid)) {
+            onlineL4DPlayer[uuid] = L4DPlayer(uuid)
+        }
+        return onlineL4DPlayer[uuid]!!
+    }
 }
