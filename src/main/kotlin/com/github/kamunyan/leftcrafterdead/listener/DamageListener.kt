e949944e476bd581131b27ca2de41524c7c73f90
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-08-28 17:51:02 +0900

    [add]Progress YOSI!

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/DamageListener.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/DamageListener.kt
@@ -1,7 +1,19 @@
 package com.github.kamunyan.leftcrafterdead.listener
 
+import com.github.kamunyan.leftcrafterdead.LeftCrafterDead
+import com.shampaggon.crackshot.events.WeaponDamageEntityEvent
+import com.shampaggon.crackshot.events.WeaponExplodeEvent
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
 class DamageListener : Listener {
-    //TODO crackshotの爆発ダメージを制御する処理を書く
+    private val plugin = LeftCrafterDead.instance
+
+    @EventHandler
+    fun onCrackShotExplosion(e: WeaponExplodeEvent) {
+    }
+
+    @EventHandler
+    fun onCrackShotDamage(e: WeaponDamageEntityEvent){
+    }
 }
