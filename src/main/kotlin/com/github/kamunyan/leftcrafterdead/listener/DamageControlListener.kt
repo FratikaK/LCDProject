75386b90bf24e7a93bbe7422e1d15a88062d4032
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-08-30 23:33:12 +0900

    [none]No progress.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/DamageControlListener.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/DamageControlListener.kt
@@ -1,19 +1,18 @@
 package com.github.kamunyan.leftcrafterdead.listener
 
 import com.github.kamunyan.leftcrafterdead.LeftCrafterDead
-import com.shampaggon.crackshot.events.WeaponDamageEntityEvent
 import com.shampaggon.crackshot.events.WeaponExplodeEvent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
-class DamageListener : Listener {
+class DamageControlListener : Listener {
     private val plugin = LeftCrafterDead.instance
 
     @EventHandler
     fun onCrackShotExplosion(e: WeaponExplodeEvent) {
-    }
-
-    @EventHandler
-    fun onCrackShotDamage(e: WeaponDamageEntityEvent){
+        val entities = e.location.getNearbyLivingEntities(5.0)
+        for (entity in entities){
+            //TODO ダメージ処理
+        }
     }
 }
