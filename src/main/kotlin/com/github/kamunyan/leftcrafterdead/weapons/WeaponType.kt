e400ba9823751f326da2142d3fea48727519a79f
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-09-11 21:18:19 +0900

    [add]Added a method to enum.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/WeaponType.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/WeaponType.kt
@@ -1,8 +1,26 @@
 package com.github.kamunyan.leftcrafterdead.weapons
 
 enum class WeaponType {
-    Primary,
-    Secondary,
-    Grenade,
-    UNKNOWN
+    Primary {
+        override fun getWeaponSlot(): Int{
+            return 0
+        }
+    },
+    Secondary {
+        override fun getWeaponSlot(): Int {
+            return 1
+        }
+    },
+    Grenade {
+        override fun getWeaponSlot(): Int {
+            return 2
+        }
+    },
+    UNKNOWN {
+        override fun getWeaponSlot(): Int {
+            return -1
+        }
+    };
+
+    abstract fun getWeaponSlot():Int
 }
