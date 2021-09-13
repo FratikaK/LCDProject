278a800383a98299a296de54162365a50c2a6d8d
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-09-13 14:53:48 +0900

    [add]Added class names.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/GunCategory.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/GunCategory.kt
@@ -1,9 +1,31 @@
 package com.github.kamunyan.leftcrafterdead.weapons
 
 enum class GunCategory {
-    ASSAULT_RIFLE,
-    SUB_MACHINE_GUN,
-    SHOTGUN,
-    HANDGUN,
-    GRENADE
+    ASSAULT_RIFLE {
+        override fun getWeaponList(): List<String> {
+            return listOf("AK12")
+        }
+    },
+    SUB_MACHINE_GUN {
+        override fun getWeaponList(): List<String> {
+            return listOf()
+        }
+    },
+    SHOTGUN {
+        override fun getWeaponList(): List<String> {
+            return listOf()
+        }
+    },
+    HANDGUN {
+        override fun getWeaponList(): List<String> {
+            return listOf()
+        }
+    },
+    GRENADE {
+        override fun getWeaponList(): List<String> {
+            return listOf("Grenade")
+        }
+    };
+
+    abstract fun getWeaponList(): List<String>
 }
