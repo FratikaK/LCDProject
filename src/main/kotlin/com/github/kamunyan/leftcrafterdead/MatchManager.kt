46837ebf646b4084cd0cbcd2758981e124dffa09
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-09-13 14:55:44 +0900

    [fix]Renamed class.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/MatchManager.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/MatchManager.kt
@@ -1,6 +1,6 @@
 package com.github.kamunyan.leftcrafterdead
 
-import com.github.kamunyan.leftcrafterdead.player.L4DPlayer
+import com.github.kamunyan.leftcrafterdead.player.LCDPlayer
 import org.bukkit.Location
 import org.bukkit.entity.Player
 import org.jetbrains.annotations.NotNull
@@ -9,14 +9,14 @@ import java.util.concurrent.ConcurrentHashMap
 object MatchManager {
 
     lateinit var lobbySpawnLocation: Location
-    val onlineL4DPlayer = ConcurrentHashMap<String, L4DPlayer>()
-    val matchPlayer = ConcurrentHashMap<String, L4DPlayer>()
+    val onlineL4DPlayer = ConcurrentHashMap<String, LCDPlayer>()
+    val matchPlayer = ConcurrentHashMap<String, LCDPlayer>()
 
     @Synchronized
-    fun getL4DPlayer(@NotNull target: Player): L4DPlayer {
+    fun getL4DPlayer(@NotNull target: Player): LCDPlayer {
         val uuid = target.uniqueId.toString()
         if (!onlineL4DPlayer.containsKey(uuid)) {
-            onlineL4DPlayer[uuid] = L4DPlayer(uuid)
+            onlineL4DPlayer[uuid] = LCDPlayer(uuid)
         }
         return onlineL4DPlayer[uuid]!!
     }
