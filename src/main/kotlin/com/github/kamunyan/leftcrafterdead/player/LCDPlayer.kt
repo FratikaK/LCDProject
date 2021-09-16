76936fd360eb67b524ef267919cd8eb9219d9922
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-09-16 16:06:00 +0900

    [add]Added init,toString.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/player/LCDPlayer.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/player/LCDPlayer.kt
@@ -7,6 +7,8 @@ import com.github.kamunyan.leftcrafterdead.perk.PerkType
 import com.github.kamunyan.leftcrafterdead.weapons.primary.PrimaryWeapon
 import com.github.kamunyan.leftcrafterdead.weapons.secondary.SecondaryWeapon
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.Material
 import org.bukkit.entity.Player
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
@@ -20,7 +22,7 @@ class LCDPlayer(uuid: String) {
 
     var isSurvivor = false
 
-    lateinit var perk: Perk
+    var perk: Perk
 
     lateinit var primary: PrimaryWeapon
 
@@ -40,11 +42,26 @@ class LCDPlayer(uuid: String) {
             PerkType.EXTERMINATOR
         )
         perkIterator.forEach { perkType -> perkLevels[perkType] = 0 }
-        val perkItem = player.inventory.getItem(8)
-        if (perkItem == null) {
-            perk = Gunslinger(perkLevels.getValue(PerkType.GUNSLINGER))
-        } else {
-        }
 
+        val perkItem = player.inventory.getItem(8)
+        perk = if (perkItem == null) {
+            Gunslinger(perkLevels.getValue(PerkType.GUNSLINGER))
+        } else {
+            when(perkItem.type){
+                Material.CROSSBOW -> Gunslinger(perkLevels.getValue(PerkType.GUNSLINGER))
+                else -> Gunslinger(perkLevels.getValue(PerkType.GUNSLINGER))
+            }
+        }
+        perk.setSymbolItem(this)
+    }
+
+    override fun toString(): String {
+        return "${ChatColor.AQUA}${player.displayName}\n" +
+                "${ChatColor.WHITE}uuid: ${player.uniqueId}\n" +
+                "isMatchPlayer: $isMatchPlayer\n" +
+                "isSurvivor: $isSurvivor\n" +
+                "perk: $perk\n" +
+                "kill: $kill\n" +
+                "perkLevels: $perkLevels\n"
     }
 }
