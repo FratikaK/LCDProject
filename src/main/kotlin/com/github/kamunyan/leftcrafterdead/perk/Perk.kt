871aec5f304c3ce93097f4f385b7c18b95f8ee02
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-09-16 16:07:48 +0900

    [add]Added Item methods.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/perk/Perk.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/perk/Perk.kt
@@ -1,9 +1,28 @@
 package com.github.kamunyan.leftcrafterdead.perk
 
+import com.github.kamunyan.leftcrafterdead.player.LCDPlayer
+import com.github.kamunyan.leftcrafterdead.util.ItemMetaUtil
 import com.github.kamunyan.leftcrafterdead.weapons.grenade.Grenade
 import com.github.kamunyan.leftcrafterdead.weapons.primary.PrimaryWeapon
+import org.bukkit.ChatColor
+import org.bukkit.Material
 
-abstract class Perk(var level: Int,val perkType: PerkType) {
+abstract class Perk(var level: Int, val perkType: PerkType) {
+    abstract fun perkSymbolMaterial(): Material
     abstract fun getGrenade(): Grenade
-    abstract fun firstPrimaryWeapon():PrimaryWeapon
+    abstract fun firstPrimaryWeapon(): PrimaryWeapon
+
+    fun setFirstWeapon(lcdPlayer: LCDPlayer) {
+        lcdPlayer.primary = firstPrimaryWeapon()
+        lcdPlayer.primary.sendWeapon(lcdPlayer.player)
+    }
+
+    fun setSymbolItem(lcdPlayer: LCDPlayer) {
+        val inventory = lcdPlayer.player.inventory
+        if (inventory.getItem(8) == null) {
+
+        }
+        val material = perkSymbolMaterial()
+        inventory.setItem(8,ItemMetaUtil.generateMetaItem(material,"${ChatColor.AQUA}${perkType.perkName}"))
+    }
 }
