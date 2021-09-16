4afbdb34b29d6d8ad01f079780b1ff11cc29027c
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-09-16 16:06:42 +0900

    [add]Added perk Material.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/perk/Gunslinger.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/perk/Gunslinger.kt
@@ -4,13 +4,18 @@ import com.github.kamunyan.leftcrafterdead.weapons.grenade.FlagGrenade
 import com.github.kamunyan.leftcrafterdead.weapons.grenade.Grenade
 import com.github.kamunyan.leftcrafterdead.weapons.primary.PrimaryWeapon
 import com.github.kamunyan.leftcrafterdead.weapons.primary.SubMachineGun
+import org.bukkit.Material
 
 class Gunslinger(level: Int) :Perk(level, PerkType.GUNSLINGER) {
+    override fun perkSymbolMaterial(): Material {
+        return Material.CROSSBOW
+    }
+
     override fun getGrenade(): Grenade {
         return FlagGrenade()
     }
 
     override fun firstPrimaryWeapon(): PrimaryWeapon {
-        return SubMachineGun("MAC10")
+        return SubMachineGun("Mac10")
     }
 }
