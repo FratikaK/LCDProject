6543a9cdf323ba3885d1bdae8cd1a52917cd82ed
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-09-19 22:57:42 +0900

    [add]VeniceConfig

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/campaign/Venice.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/campaign/Venice.kt
@@ -1,17 +1,19 @@
 package com.github.kamunyan.leftcrafterdead.campaign
 
+import com.github.kamunyan.leftcrafterdead.configs.Config
 import com.github.kamunyan.leftcrafterdead.configs.campaign.VeniceConfig
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.WorldCreator
 import org.bukkit.WorldType
 
-class Venice: Campaign {
+class Venice : Campaign {
 
     override val campaignTitle: String = "Venice"
     override val campaignSymbol: Material = Material.BRICKS
     override val startLocation: Location = VeniceConfig.startLocation
     override val restLocation: Location = VeniceConfig.restLocation
+    override val config: Config = VeniceConfig
 
     override fun createMapWorld() {
         WorldCreator(campaignTitle).type(WorldType.NORMAL).createWorld()
