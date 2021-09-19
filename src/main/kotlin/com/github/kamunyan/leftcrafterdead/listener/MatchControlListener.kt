66a5b555952192086d98f7037189dedd1697b190
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-09-19 22:56:22 +0900

    [add]onMatchStart

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/MatchControlListener.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/MatchControlListener.kt
@@ -1,7 +1,23 @@
 package com.github.kamunyan.leftcrafterdead.listener
 
+import com.github.kamunyan.leftcrafterdead.LeftCrafterDead
+import com.github.kamunyan.leftcrafterdead.MatchManager
+import com.github.kamunyan.leftcrafterdead.event.MatchStartEvent
+import org.bukkit.ChatColor
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
 class MatchControlListener:Listener {
+    private val plugin = LeftCrafterDead.instance
+    private val manager = MatchManager
 
+    @EventHandler
+    fun onMatchStart(e: MatchStartEvent){
+        if (manager.isMatch){
+            plugin.logger.info("${ChatColor.RED}[LCD]すでにマッチを開始しています")
+            return
+        }else {
+            manager.isMatch = true
+        }
+    }
 }
