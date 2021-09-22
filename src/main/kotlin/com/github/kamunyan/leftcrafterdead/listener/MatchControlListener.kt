060d5a889afbcdd6495ef73a146c0b3cd1d83d1e
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-09-22 23:29:18 +0900

    [add]onLobbyItemRightInteract

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/MatchControlListener.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/MatchControlListener.kt
@@ -4,21 +4,39 @@ import com.github.kamunyan.leftcrafterdead.LeftCrafterDead
 import com.github.kamunyan.leftcrafterdead.MatchManager
 import com.github.kamunyan.leftcrafterdead.event.MatchStartEvent
 import org.bukkit.ChatColor
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerInteractEvent
 
-class MatchControlListener:Listener {
+class MatchControlListener : Listener {
     private val plugin = LeftCrafterDead.instance
     private val manager = MatchManager
 
     @EventHandler
-    fun onMatchStart(e: MatchStartEvent){
-        if (manager.isMatch){
+    fun onMatchStart(e: MatchStartEvent) {
+        if (manager.isMatch) {
             plugin.logger.info("${ChatColor.RED}[LCD]すでにマッチを開始しています")
             return
-        }else {
+        } else {
             manager.isMatch = true
         }
         manager.startCampaign()
+    }
+
+    @EventHandler
+    fun onLobbyItemRightInteract(e: PlayerInteractEvent) {
+        if (e.action == Action.LEFT_CLICK_AIR || e.action == Action.LEFT_CLICK_BLOCK) {
+            return
+        }
+
+        if (e.item != null && e.item!!.hasItemMeta()) {
+            val item = e.item!!
+            when (item.type) {
+                Material.DIAMOND -> manager.joinPlayer(e.player)
+                else -> return
+            }
+        }
     }
 }
