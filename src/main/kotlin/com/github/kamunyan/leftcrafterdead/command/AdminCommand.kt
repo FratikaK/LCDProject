9fd97898d222b2b695a332a08f158720d1349c6b
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-09-25 17:40:27 +0900

    [add]removeEntity

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/command/AdminCommand.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/command/AdminCommand.kt
@@ -11,44 +11,46 @@ import org.bukkit.entity.Player
 
 class AdminCommand : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if (command.name != "admin") return false
+        var flag = false
+        if (command.name != "admin") return flag
         if (sender is Player) {
             sender.sendMessage("${ChatColor.RED}コンソールから実行してください")
-            return false
-        }
-        if (args.isEmpty()) {
-            sendAdminCommandInfo(sender)
-            return false
         }
 
         when (args[0]) {
             "info" -> {
                 if (args.size == 2) {
-                    val player = Bukkit.getPlayer(args[1]) ?: return false
-                    if (MatchManager.onlineL4DPlayer.containsKey(player.uniqueId.toString())) {
+                    val player = Bukkit.getPlayer(args[1])
+                    if (player != null && MatchManager.onlineL4DPlayer.containsKey(player.uniqueId.toString())) {
                         val lcdPlayer = MatchManager.onlineL4DPlayer[player.uniqueId.toString()]
                         sender.sendMessage(lcdPlayer.toString())
-                        return true
+                        flag = true
                     }
                 }
             }
+            "removeEntity" -> {
+                MatchManager.deleteEnemyMob()
+                flag = true
+            }
             "reload" -> {
                 LeftCrafterDead.instance.server.reload()
-                return true
+                flag = true
             }
             else -> {
-                sendAdminCommandInfo(sender)
-                return false
             }
         }
-        return false
+        if (!flag) {
+            sendAdminCommandInfo(sender)
+        }
+        return flag
     }
 
     private fun sendAdminCommandInfo(sender: CommandSender) {
         sender.sendMessage(
             "admin Command usage\n" +
                     "admin info [player name]: LCDPlayerの詳細を返します\n" +
-                    "admin info [reload]: サーバをリロードします"
+                    "admin info [reload]: サーバをリロードします\n" +
+                    "admin removeEntity: マップの敵性mobを削除します"
         )
     }
 }
