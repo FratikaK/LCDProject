04d9fb8775fad7a8e342e063c8eb996b5ae2606d
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-09-28 20:57:05 +0900

    [add]Implemented Cancellable.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/event/RushStartEvent.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/event/RushStartEvent.kt
@@ -1,9 +1,12 @@
 package com.github.kamunyan.leftcrafterdead.event
 
+import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
-class RushStartEvent : Event() {
+class RushStartEvent : Event(), Cancellable {
+    private var cancel = false
+
     override fun getHandlers(): HandlerList {
         return HANDLERS
     }
@@ -15,5 +18,13 @@ class RushStartEvent : Event() {
         fun getHandlerList(): HandlerList {
             return HANDLERS
         }
+    }
+
+    override fun isCancelled(): Boolean {
+        return this.cancel
+    }
+
+    override fun setCancelled(cancel: Boolean) {
+        this.cancel = cancel
     }
 }
