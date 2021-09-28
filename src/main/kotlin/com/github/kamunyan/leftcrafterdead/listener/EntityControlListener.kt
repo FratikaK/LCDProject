c6f0b871af9d2fa18573fe2f0a1ffab01e3684ec
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-09-28 20:54:04 +0900

    [add]onEntityTargetMob,onPlayerDeath,onPlayerRespawn

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/EntityControlListener.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/EntityControlListener.kt
@@ -2,16 +2,15 @@ package com.github.kamunyan.leftcrafterdead.listener
 
 import com.github.kamunyan.leftcrafterdead.LeftCrafterDead
 import com.github.kamunyan.leftcrafterdead.MatchManager
+import com.github.kamunyan.leftcrafterdead.event.LCDPlayerDeathEvent
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.entity.CreatureSpawnEvent
-import org.bukkit.event.entity.EntityTargetEvent
-import org.bukkit.event.entity.EntityTargetLivingEntityEvent
-import org.bukkit.event.entity.FoodLevelChangeEvent
+import org.bukkit.event.entity.*
+import org.bukkit.event.player.PlayerRespawnEvent
 import java.util.*
 
-class EntityControlListener :Listener{
+class EntityControlListener : Listener {
     private val plugin = LeftCrafterDead.instance
     private val manager = MatchManager
 
@@ -19,21 +18,64 @@ class EntityControlListener :Listener{
      * スポーンする敵性Mobの初期設定を行う
      */
     @EventHandler
-    fun onLivingEntitySpawn(e: CreatureSpawnEvent){
+    fun onLivingEntitySpawn(e: CreatureSpawnEvent) {
         val entity = e.entity
         entity.noDamageTicks = 0
         entity.removeWhenFarAway = false
 
         //ラッシュ用mob設定
-        if (e.spawnReason == CreatureSpawnEvent.SpawnReason.JOCKEY){
+        if (e.spawnReason == CreatureSpawnEvent.SpawnReason.JOCKEY) {
             val random = Random().nextInt(manager.matchPlayer.size)
             val player = manager.matchPlayer[random - 1].player
-            Bukkit.getPluginManager().callEvent(EntityTargetLivingEntityEvent(entity,player,EntityTargetEvent.TargetReason.CLOSEST_PLAYER))
+            Bukkit.getPluginManager()
+                .callEvent(EntityTargetLivingEntityEvent(entity, player, EntityTargetEvent.TargetReason.CLOSEST_PLAYER))
         }
     }
 
     @EventHandler
-    fun onFoodLevelChange(e:FoodLevelChangeEvent){
+    fun onEntityTargetMob(e: EntityTargetLivingEntityEvent) {
+        if (e.reason == EntityTargetEvent.TargetReason.TARGET_ATTACKED_NEARBY_ENTITY) {
+            e.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun onPlayerDeath(e: PlayerDeathEvent) {
+        val lcdPlayer = manager.getLCDPlayer(e.entity.uniqueId)
+
+        if (manager.isMatchPlayer(lcdPlayer) && manager.isMatch) {
+            if (lcdPlayer.isSurvivor) {
+                Bukkit.getPluginManager().callEvent(LCDPlayerDeathEvent(lcdPlayer))
+            }
+        }
+        e.deathMessage = ""
+    }
+
+    @EventHandler
+    fun onPlayerRespawn(e: PlayerRespawnEvent) {
+        val lcdPlayer = manager.getLCDPlayer(e.player)
+        if (manager.isMatch) {
+            if (!lcdPlayer.isSurvivor) {
+                if (manager.numberOfSurvivors() > 1) {
+                    for (target in manager.matchPlayer) {
+                        if (target.isSurvivor) {
+                            e.respawnLocation = target.player.location
+                            break
+                        }
+                    }
+                } else {
+                    e.respawnLocation = manager.campaign.startLocation
+                }
+                lcdPlayer.setSpectator()
+            }
+        } else if (!manager.isMatch) {
+            e.respawnLocation = manager.lobbySpawnLocation
+            lcdPlayer.setLobbyItem()
+        }
+    }
+
+    @EventHandler
+    fun onFoodLevelChange(e: FoodLevelChangeEvent) {
         e.isCancelled = true
     }
 }
