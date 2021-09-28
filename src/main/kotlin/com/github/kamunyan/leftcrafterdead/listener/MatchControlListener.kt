b7a9210fcda70f91c24ccb405eb7447bfee271a3
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-09-28 20:55:36 +0900

    [fix]IllegalArgumentException

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/MatchControlListener.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/MatchControlListener.kt
@@ -2,6 +2,7 @@ package com.github.kamunyan.leftcrafterdead.listener
 
 import com.github.kamunyan.leftcrafterdead.LeftCrafterDead
 import com.github.kamunyan.leftcrafterdead.MatchManager
+import com.github.kamunyan.leftcrafterdead.event.LCDPlayerDeathEvent
 import com.github.kamunyan.leftcrafterdead.event.MatchStartEvent
 import com.github.kamunyan.leftcrafterdead.event.RushStartEvent
 import org.bukkit.ChatColor
@@ -13,6 +14,7 @@ import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.entity.CreatureSpawnEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import java.lang.IllegalArgumentException
 import java.util.*
 
 class MatchControlListener : Listener {
@@ -46,41 +48,65 @@ class MatchControlListener : Listener {
     }
 
     @EventHandler
+    fun onLCDPlayerDeath(e: LCDPlayerDeathEvent) {
+        val lcdPlayer = e.lcdPlayer
+        lcdPlayer.isSurvivor = false
+        if (manager.numberOfSurvivors() <= 0){
+            manager.finishCampaign()
+        }
+    }
+
+    @EventHandler
     fun onRushStart(e: RushStartEvent) {
         if (manager.matchPlayer.isNotEmpty()) {
             //ランダムにプレイヤーを決定する
             val random = Random().nextInt(manager.matchPlayer.size)
-            val player = manager.matchPlayer[random - 1].player
+            val player = manager.matchPlayer[random].player
             val playerLocation = player.location.clone()
 
             //プレイヤーの座標から一番近いmobスポーンポイントにスポーンさせる
             var minLocation: Location? = null
             var minDistance: Double? = null
-            manager.mobSpawnLocationList.forEach { location ->
-                if (minLocation == null) {
-                    minLocation = location
-                    minDistance = location.distance(playerLocation)
-                    return@forEach
+
+            try {
+                manager.mobSpawnLocationList.forEach { location ->
+                    if (minLocation == null) {
+                        minLocation = location
+                        minDistance = location.distance(playerLocation)
+                        return@forEach
+                    }
+                    val distance = location.distance(playerLocation)
+                    if (distance < minDistance!!) {
+                        minLocation = location
+                        minDistance = distance
+                    }
                 }
-                val distance = location.distance(playerLocation)
-                if (distance < minDistance!!) {
-                    minLocation = location
-                    minDistance = distance
+                if (manager.campaign.world != null) {
+                    if (minLocation == null) {
+                        plugin.logger.info("[onRushStart]${ChatColor.RED}minLocation is Null!")
+                        return
+                    }
+                    for (i in 0..(manager.matchPlayer.size * manager.campaign.determiningDifficulty().normalMobSpawnAmount)) {
+                        val locations = arrayOf(
+                            minLocation,
+                            minLocation!!.clone().add(1.0, 0.0, 0.0),
+                            minLocation!!.clone().add(-1.0, 0.0, 0.0),
+                            minLocation!!.clone().add(0.0, 0.0, 1.0),
+                            minLocation!!.clone().add(0.0, 0.0, -1.0)
+                        )
+                        locations.forEach { location ->
+                            manager.campaign.world!!.spawnEntity(
+                                minLocation!!,
+                                EntityType.ZOMBIE,
+                                CreatureSpawnEvent.SpawnReason.CUSTOM
+                            )
+                        }
+                    }
+                    plugin.logger.info("[onRushStart]${ChatColor.AQUA}RushMobがスポーンしました")
                 }
-            }
-            if (manager.campaign.world != null) {
-                if (minLocation == null) {
-                    plugin.logger.info("[onRushStart]${ChatColor.RED}minLocation is Null!")
-                    return
-                }
-                for (i in 0..(manager.matchPlayer.size * manager.campaign.determiningDifficulty().normalMobSpawnAmount)) {
-                    manager.campaign.world!!.spawnEntity(
-                        minLocation!!,
-                        EntityType.ZOMBIE,
-                        CreatureSpawnEvent.SpawnReason.JOCKEY
-                    )
-                }
-                plugin.logger.info("[onRushStart]${ChatColor.AQUA}RushMobがスポーンしました")
+            } catch (exception: IllegalArgumentException) {
+                plugin.logger.info("[onRushStart]Canceled MatchStartEvent.")
+                e.isCancelled = true
             }
         }
     }
