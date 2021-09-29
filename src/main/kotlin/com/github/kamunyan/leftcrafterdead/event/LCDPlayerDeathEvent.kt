d62e36d043c84cb12022bc016b3c5a23e2237ba8
FratikaK <kamunyan17@gmail.com>
2021-09-29 12:20:11 +0900

    [add]Constructor parameters.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/event/LCDPlayerDeathEvent.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/event/LCDPlayerDeathEvent.kt
@@ -1,10 +1,11 @@
 package com.github.kamunyan.leftcrafterdead.event
 
 import com.github.kamunyan.leftcrafterdead.player.LCDPlayer
+import org.bukkit.entity.EntityType
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
-class LCDPlayerDeathEvent(val lcdPlayer: LCDPlayer) : Event() {
+class LCDPlayerDeathEvent(val lcdPlayer: LCDPlayer, var entityType: EntityType) : Event() {
     override fun getHandlers(): HandlerList {
         return HANDLERS
     }
