bb1bd0399e3c13d143cace54d241aa37b206dc92
FratikaK <kamunyan17@gmail.com>
2021-09-30 14:35:15 +0900

    [add]HealGrenade

---
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/grenade/HealGrenade.kt
@@ -0,0 +1,11 @@
+package com.github.kamunyan.leftcrafterdead.weapons.grenade
+
+import org.bukkit.entity.Entity
+
+class HealGrenade : Grenade("Heal Grenade") {
+    override fun loadWeaponCapabilities() {
+    }
+
+    override fun specialEffects(entity: Entity) {
+    }
+}
