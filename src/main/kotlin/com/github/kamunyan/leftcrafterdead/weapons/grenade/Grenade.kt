2c72e106e2e6c25a3cef4a3cd74d1d40b3745050
FratikaK <kamunyan17@gmail.com>
2021-10-05 14:56:21 +0900

    [fix]moved sendGrenade

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/grenade/Grenade.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/grenade/Grenade.kt
@@ -3,9 +3,22 @@ package com.github.kamunyan.leftcrafterdead.weapons.grenade
 import com.github.kamunyan.leftcrafterdead.weapons.GunCategory
 import com.github.kamunyan.leftcrafterdead.weapons.LCDWeapon
 import com.github.kamunyan.leftcrafterdead.weapons.WeaponType
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 
 abstract class Grenade(weaponTitle: String) : LCDWeapon(weaponTitle, WeaponType.Grenade) {
     override fun getGunCategory(): GunCategory {
         return GunCategory.GRENADE
+    }
+
+    fun sendGrenade(player: Player, amount: Int) {
+        if (weaponType != WeaponType.Grenade) {
+            return
+        }
+        val grenade = crackShot.generateWeapon(weaponTitle)
+        val itemMeta = grenade.itemMeta
+        val itemStack = ItemStack(grenade.type, amount)
+        itemStack.itemMeta = itemMeta
+        player.inventory.setItem(2, itemStack)
     }
 }
