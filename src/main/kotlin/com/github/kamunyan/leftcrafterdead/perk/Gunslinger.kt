49c4697c0ec4a682dee9c03cd99a1928e69c8e03
FratikaK <kamunyan17@gmail.com>
2021-10-05 14:57:21 +0900

    [fix]firstPrimaryWeapon

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/perk/Gunslinger.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/perk/Gunslinger.kt
@@ -2,6 +2,7 @@ package com.github.kamunyan.leftcrafterdead.perk
 
 import com.github.kamunyan.leftcrafterdead.weapons.grenade.FlagGrenade
 import com.github.kamunyan.leftcrafterdead.weapons.grenade.Grenade
+import com.github.kamunyan.leftcrafterdead.weapons.primary.AssaultRifle
 import com.github.kamunyan.leftcrafterdead.weapons.primary.PrimaryWeapon
 import com.github.kamunyan.leftcrafterdead.weapons.primary.SubMachineGun
 import org.bukkit.Material
@@ -16,6 +17,6 @@ class Gunslinger : Perk(PerkType.GUNSLINGER) {
     }
 
     override fun firstPrimaryWeapon(): PrimaryWeapon {
-        return SubMachineGun("AK12")
+        return AssaultRifle("AK12")
     }
 }
