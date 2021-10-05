e2a9c45e86e358ad60908ffcff33e1b0dc200164
FratikaK <kamunyan17@gmail.com>
2021-10-05 14:55:08 +0900

    [add]StartCheckPointEvent

---
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/event/StartCheckPointEvent.kt
@@ -0,0 +1,19 @@
+package com.github.kamunyan.leftcrafterdead.event
+
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+
+class StartCheckPointEvent : Event() {
+    override fun getHandlers(): HandlerList {
+        return HANDLERS
+    }
+
+    companion object {
+        private val HANDLERS = HandlerList()
+
+        @JvmStatic
+        fun getHandlerList(): HandlerList {
+            return HANDLERS
+        }
+    }
+}
