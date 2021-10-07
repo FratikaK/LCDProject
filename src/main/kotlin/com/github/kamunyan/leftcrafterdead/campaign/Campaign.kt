099e4e7e9b4df2d41f86d67e8b9adcc9c75cf1e9
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-10-07 21:22:54 +0900

    [add]BossBar

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/campaign/Campaign.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/campaign/Campaign.kt
@@ -5,8 +5,12 @@ import com.github.kamunyan.leftcrafterdead.MatchManager
 import com.github.kamunyan.leftcrafterdead.configs.Config
 import com.github.kamunyan.leftcrafterdead.event.RushStartEvent
 import org.bukkit.*
+import org.bukkit.boss.BarColor
+import org.bukkit.boss.BarFlag
+import org.bukkit.boss.BarStyle
 import org.bukkit.entity.EntityType
 import org.bukkit.scheduler.BukkitRunnable
+import kotlin.math.floor
 
 interface Campaign {
     val plugin: LeftCrafterDead
@@ -36,28 +40,30 @@ interface Campaign {
     fun determiningDifficulty(): Difficulty
 
     fun startRush() {
+        val bossBar = Bukkit.createBossBar("Time to Rush", BarColor.RED, BarStyle.SEGMENTED_10, BarFlag.CREATE_FOG)
+        manager.matchPlayer.forEach { lcdPlayer ->
+            bossBar.addPlayer(lcdPlayer.player)
+        }
+        bossBar.isVisible = true
         object : BukkitRunnable() {
-            var timeLeft = 60
+            var timeLeft = 1.0
             override fun run() {
                 if (manager.isCheckPoint || !manager.isMatch) {
                     plugin.logger.info("[startRush]${ChatColor.AQUA}ラッシュのタイマーを停止しました")
+                    bossBar.removeAll()
+                    bossBar.isVisible = false
                     this.cancel()
                     return
                 }
 
-                if (timeLeft == 30) {
-                    plugin.logger.info("[startRush]${ChatColor.AQUA}次のラッシュまで30秒")
-                }
-
-                if (timeLeft == 10) {
-                    plugin.logger.info("[startRush]${ChatColor.AQUA}次のラッシュまで10秒")
-                }
-
                 if (timeLeft <= 0) {
                     Bukkit.getPluginManager().callEvent(RushStartEvent())
-                    timeLeft = 60
+                    timeLeft = 1.0
                 }
-                timeLeft -= 1
+
+                timeLeft -= 0.02
+                if (timeLeft < 0) return
+                bossBar.progress = timeLeft
             }
         }.runTaskTimer(plugin, 0, 20)
     }
