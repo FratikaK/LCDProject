59b76c5c46a91c716e063b07f3e47187242ca2e1
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-10-22 22:42:31 +0900

    [add]gadgetRightInteract

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/perk/Gunslinger.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/perk/Gunslinger.kt
@@ -1,12 +1,20 @@
 package com.github.kamunyan.leftcrafterdead.perk
 
+import com.github.kamunyan.leftcrafterdead.LeftCrafterDead
 import com.github.kamunyan.leftcrafterdead.player.LCDPlayer
+import com.github.kamunyan.leftcrafterdead.util.ItemMetaUtil
 import com.github.kamunyan.leftcrafterdead.weapons.grenade.FlagGrenade
 import com.github.kamunyan.leftcrafterdead.weapons.grenade.Grenade
 import com.github.kamunyan.leftcrafterdead.weapons.primary.AssaultRifle
 import com.github.kamunyan.leftcrafterdead.weapons.primary.PrimaryWeapon
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.Material
+import org.bukkit.boss.BarColor
+import org.bukkit.boss.BarFlag
+import org.bukkit.boss.BarStyle
 import org.bukkit.inventory.ItemStack
+import org.bukkit.scheduler.BukkitRunnable
 
 class Gunslinger : Perk(PerkType.GUNSLINGER) {
     override fun perkSymbolMaterial(): Material {
@@ -22,11 +30,41 @@ class Gunslinger : Perk(PerkType.GUNSLINGER) {
     }
 
     override fun perkGadgetItem(): ItemStack {
-        TODO("Not yet implemented")
+        return ItemMetaUtil.generateMetaItem(
+            Material.TRIPWIRE_HOOK,
+            "${ChatColor.GOLD}Weapon Boost",
+            110,
+            listOf(
+                "20秒間フルオート武器の", "発射レート倍率を3段階",
+                "上昇させ、リロードのスピードを50%上昇させる"
+            )
+        )
     }
 
     override fun gadgetRightInteract(lcdPlayer: LCDPlayer) {
-
+        startGadgetStartCoolDown(lcdPlayer)
+        val rateQuantity = 3
+        val reloadSpeed = 0.5
+        lcdPlayer.rateAcceleration = lcdPlayer.rateAcceleration + rateQuantity
+        lcdPlayer.reloadSpeedAcceleration += reloadSpeed
+        val bossBar = Bukkit.createBossBar("レート上昇中", BarColor.PURPLE, BarStyle.SEGMENTED_10, BarFlag.CREATE_FOG)
+        bossBar.addPlayer(lcdPlayer.player)
+        bossBar.isVisible = true
+        var timeLeft = 1.0
+        object : BukkitRunnable() {
+            override fun run() {
+                if (timeLeft <= 0.0){
+                    bossBar.removeAll()
+                    bossBar.isVisible = false
+                    lcdPlayer.rateAcceleration -= rateQuantity
+                    lcdPlayer.reloadSpeedAcceleration -= reloadSpeed
+                    cancel()
+                    return
+                }
+                bossBar.progress = timeLeft
+                timeLeft -= 0.05
+            }
+        }.runTaskTimerAsynchronously(LeftCrafterDead.instance, 0, 20)
     }
 
     override val gadgetCoolDown: Int = 40
