e246dd27a0377629b3783ff17e0dff087cef9bf8
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-11-07 23:33:48 +0900

    [fix]fixed armor bug.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/perk/Fixer.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/perk/Fixer.kt
@@ -1,20 +1,14 @@
 package com.github.kamunyan.leftcrafterdead.perk
 
-import com.github.kamunyan.leftcrafterdead.LeftCrafterDead
 import com.github.kamunyan.leftcrafterdead.player.LCDPlayer
 import com.github.kamunyan.leftcrafterdead.util.ItemMetaUtil
 import com.github.kamunyan.leftcrafterdead.weapons.grenade.Concussion
 import com.github.kamunyan.leftcrafterdead.weapons.grenade.Grenade
 import com.github.kamunyan.leftcrafterdead.weapons.primary.AssaultRifle
 import com.github.kamunyan.leftcrafterdead.weapons.primary.PrimaryWeapon
-import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.Material
-import org.bukkit.boss.BarColor
-import org.bukkit.boss.BarFlag
-import org.bukkit.boss.BarStyle
 import org.bukkit.inventory.ItemStack
-import org.bukkit.scheduler.BukkitRunnable
 
 class Fixer : Perk(PerkType.FIXER) {
     override fun perkSymbolMaterial(): Material {
@@ -36,32 +30,16 @@ class Fixer : Perk(PerkType.FIXER) {
             110,
             listOf(
                 "半径10mにいるプレイヤーに、",
-                "一時的にアーマーを付与する"
+                "最大体力の60%のアーマーを付与する"
             )
         )
     }
 
     override fun gadgetRightInteract(lcdPlayer: LCDPlayer) {
         startGadgetStartCoolDown(lcdPlayer)
-        val armorAmount = 6
-        val bossBar = Bukkit.createBossBar("Armor Boost", BarColor.YELLOW, BarStyle.SEGMENTED_10, BarFlag.CREATE_FOG)
         lcdPlayer.player.location.getNearbyPlayers(10.0).forEach { player ->
-            bossBar.addPlayer(player)
-            bossBar.isVisible = true
+            val armorAmount = player.healthScale * 0.6
             player.absorptionAmount = player.absorptionAmount + armorAmount
-            object : BukkitRunnable() {
-                var timeLeft = 1.0
-                override fun run() {
-                    if (timeLeft <= 0) {
-                        player.absorptionAmount = player.absorptionAmount - armorAmount
-                        bossBar.isVisible = false
-                        bossBar.removePlayer(player)
-                        cancel()
-                        return
-                    }
-                    timeLeft -= 0.02
-                }
-            }.runTaskTimerAsynchronously(LeftCrafterDead.instance, 0, 20)
         }
     }
 
