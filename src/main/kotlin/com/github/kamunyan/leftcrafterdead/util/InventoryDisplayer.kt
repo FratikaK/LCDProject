3fa61762fa18f97f12d357660bfdf6e647434022
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-11-07 23:27:12 +0900

    [add]added class item.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/util/InventoryDisplayer.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/util/InventoryDisplayer.kt
@@ -1,6 +1,5 @@
 package com.github.kamunyan.leftcrafterdead.util
 
-import com.github.kamunyan.leftcrafterdead.perk.PerkType
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.Material
@@ -24,9 +23,23 @@ object InventoryDisplayer {
             100,
             listOf("チームを治癒で支えるPerk","自身、味方の体力を回復させる武器、アイテムを所持する")
         )
+        val fixer = util.generateMetaItem(
+            Material.HONEYCOMB,
+            "${ChatColor.AQUA}Fixer",
+            100,
+            listOf("味方をサポートするPerk","敵、味方に対して足止め、補助をするアイテムを所持する")
+        )
+        val exterminator = util.generateMetaItem(
+            Material.NETHERITE_INGOT,
+            "${ChatColor.AQUA}Exterminator",
+            100,
+            listOf("対集団に特化したPerk","敵集団に対して有用なアイテムを所持する")
+        )
 
         inventory.setItem(0,gunslinger)
-        inventory.setItem(2,medic)
+        inventory.setItem(1,medic)
+        inventory.setItem(2,fixer)
+        inventory.setItem(3,exterminator)
 
         return inventory
     }
