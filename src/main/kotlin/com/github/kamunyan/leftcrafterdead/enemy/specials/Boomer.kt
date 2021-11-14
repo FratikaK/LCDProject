64324feb5db81ebf14815bdbc5eaaea2eb034ad2
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-11-14 23:12:54 +0900

    Added broadcast message.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/enemy/specials/Boomer.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/enemy/specials/Boomer.kt
@@ -1,20 +1,21 @@
 package com.github.kamunyan.leftcrafterdead.enemy.specials
 
-import com.github.kamunyan.leftcrafterdead.enemy.LCDEnemy
+import com.github.kamunyan.leftcrafterdead.event.RushStartEvent
+import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.Sound
 import org.bukkit.entity.EntityType
+import org.bukkit.entity.LivingEntity
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 import xyz.xenondevs.particle.ParticleBuilder
 import xyz.xenondevs.particle.ParticleEffect
-import java.util.*
 
-class Boomer : LCDEnemy() {
-    override lateinit var uuid: UUID
-    override val metadataKey: String = SPECIAL_ENEMY_KEY
+object Boomer : LCDSpecialEnemy() {
     override val entityType: EntityType = EntityType.CREEPER
-    override val enemyType: EnemyType = EnemyType.SPECIAL
+    override fun specialEnemyRunnable(livingEntity: LivingEntity) {}
+
     override val nonHeadShotDamageResistance: Double = 0.0
     override val explosionResistance: Double = 0.0
 
@@ -26,16 +27,20 @@ class Boomer : LCDEnemy() {
         return 1.0
     }
 
-    override fun enemyDeathEffects() {
-        val entity = Bukkit.getEntity(uuid) ?: return
-        val location = entity.location.clone()
-        location.getNearbyPlayers(4.0).forEach { player ->
-            player.addPotionEffects(
-                listOf(
-                    PotionEffect(PotionEffectType.SLOW, 200, 2, false, false),
-                    PotionEffect(PotionEffectType.BLINDNESS, 200, 5, false, false)
+    override fun enemyDeathEffects(enemy: LivingEntity) {
+        val location = enemy.location.clone()
+        val players = location.getNearbyPlayers(4.0)
+        if (players.isNotEmpty()) {
+            Bukkit.getPluginManager().callEvent(RushStartEvent())
+            players.forEach { player ->
+                player.addPotionEffects(
+                    listOf(
+                        PotionEffect(PotionEffectType.SLOW, 200, 2, false, false),
+                        PotionEffect(PotionEffectType.BLINDNESS, 200, 5, false, false)
+                    )
                 )
-            )
+                Bukkit.broadcast(Component.text("${ChatColor.AQUA}${player.name}${ChatColor.YELLOW}が敵をおびき寄せてしまった！"))
+            }
         }
         location.world.playSound(location, Sound.ENTITY_GENERIC_EXPLODE, 2f, 3f)
         ParticleBuilder(ParticleEffect.EXPLOSION_LARGE, location).display()
