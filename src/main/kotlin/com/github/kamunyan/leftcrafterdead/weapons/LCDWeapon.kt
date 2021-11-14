8fefc61d7d91601242d3a1a1c4cb54874aeb24bc
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-11-14 23:25:43 +0900

    Added weaponDataList.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/LCDWeapon.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/LCDWeapon.kt
@@ -1,13 +1,31 @@
 package com.github.kamunyan.leftcrafterdead.weapons
 
 import com.github.kamunyan.leftcrafterdead.LeftCrafterDead
+import org.bukkit.ChatColor
 import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
 abstract class LCDWeapon(val weaponTitle: String, val weaponType: WeaponType) {
 
+    companion object {
+        const val DAMAGE = ".Shooting.Projectile_Damage"
+        const val SPEED = ".Shooting.Projectile_Speed"
+        const val SPREAD = ".Shooting.Bullet_Spread"
+        const val AMOUNT = ".Shooting.Projectile_Amount"
+        const val FIRE_RATE = ".Fully_Automatic.Fire_Rate"
+        const val RELOAD_AMOUNT = ".Reload.Reload_Amount"
+        const val RELOAD_DURATION = ".Reload.Reload_Duration"
+        const val HEADSHOT_DAMAGE = ".Headshot.Bonus_Damage"
+    }
+
     val crackShot = LeftCrafterDead.instance.crackShot
+
+    fun weaponDataInfo(infoName: String, node: String): String {
+        return "${ChatColor.YELLOW}${infoName}${ChatColor.WHITE}: " +
+                "${ChatColor.RED}${crackShot.handle.getInt(weaponTitle + node)}"
+    }
+
 
     fun sendWeapon(player: Player) {
         if (weaponType == WeaponType.Primary) {
@@ -24,4 +42,6 @@ abstract class LCDWeapon(val weaponTitle: String, val weaponType: WeaponType) {
     abstract fun getGunCategory(): GunCategory
 
     abstract fun specialEffects(attacker: Player, entity: LivingEntity)
+
+    abstract fun weaponDataList(): List<String>
 }
--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/secondary/SecondaryWeapon.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/secondary/SecondaryWeapon.kt
@@ -4,4 +4,7 @@ import com.github.kamunyan.leftcrafterdead.weapons.LCDWeapon
 import com.github.kamunyan.leftcrafterdead.weapons.WeaponType
 
 abstract class SecondaryWeapon(weaponTitle: String, weaponType: WeaponType) :LCDWeapon(weaponTitle, weaponType) {
+    override fun weaponDataList(): List<String> {
+        return listOf()
+    }
 }
