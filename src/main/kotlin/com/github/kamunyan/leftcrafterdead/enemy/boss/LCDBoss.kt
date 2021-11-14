ed111c47e9d5c502f28d670bd62df03b6f89f3d2
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-11-14 23:24:23 +0900

    Added Boss class.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/enemy/boss/LCDBoss.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/enemy/boss/LCDBoss.kt
@@ -1,16 +1,56 @@
 package com.github.kamunyan.leftcrafterdead.enemy.boss
 
+import com.github.kamunyan.leftcrafterdead.LeftCrafterDead
 import com.github.kamunyan.leftcrafterdead.enemy.LCDEnemy
+import com.github.kamunyan.leftcrafterdead.event.DefeatBossEvent
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.entity.LivingEntity
+import org.bukkit.event.entity.CreatureSpawnEvent
+import org.bukkit.scheduler.BukkitRunnable
+import java.util.*
 
 abstract class LCDBoss : LCDEnemy() {
     override val metadataKey: String = BOSS_ENEMY_KEY
     override val enemyType: EnemyType = EnemyType.BOSS
+    lateinit var livingEntity: LivingEntity
+    abstract val bossSkills: List<BossSkill>
+
+    override fun spawnEnemy(location: Location) {
+        val enemy = manager.world.spawnEntity(location, entityType, CreatureSpawnEvent.SpawnReason.CUSTOM)
+        setMetadata(enemy)
+        manager.enemyHashMap[enemy.uniqueId] = this
+        livingEntity = enemy as LivingEntity
+        setLivingEntitySettings(livingEntity)
+        bossNormalAttackRunnable()
+        bossSkillRunnable()
+    }
+
+    abstract fun bossNormalAttackRunnable()
+
+    private fun bossSkillRunnable() {
+        var timeLeft = bossSkillCoolDown
+        val random = Random()
+        object : BukkitRunnable() {
+            override fun run() {
+                if (livingEntity.isDead) {
+                    cancel()
+                    return
+                }
+                if (timeLeft <= 0) {
+                    timeLeft = bossSkillCoolDown
+                    Bukkit.getLogger().info("ボススキル発動！")
+                    bossSkills[random.nextInt(bossSkills.size)].activationSkill(livingEntity)
+                    return
+                }
+                timeLeft--
+            }
+        }.runTaskTimer(LeftCrafterDead.instance, 0, 20)
+    }
+
+    override fun enemyDeathEffects(enemy: LivingEntity) {
+        Bukkit.getPluginManager().callEvent(DefeatBossEvent())
+    }
 
     abstract val bossSkillCoolDown: Int
-
-    abstract fun bossSkillRunnable()
-
-    abstract fun bossSkill()
-
-    abstract fun bossDamagedRunnable()
 }
---
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/enemy/boss/MasterSmoker.kt
@@ -0,0 +1,133 @@
+package com.github.kamunyan.leftcrafterdead.enemy.boss
+
+import com.github.kamunyan.leftcrafterdead.LeftCrafterDead
+import com.github.kamunyan.leftcrafterdead.campaign.Campaign
+import com.github.kamunyan.leftcrafterdead.util.MetadataUtil
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.Sound
+import org.bukkit.attribute.Attribute
+import org.bukkit.entity.Arrow
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.LivingEntity
+import org.bukkit.inventory.ItemStack
+import org.bukkit.scheduler.BukkitRunnable
+import org.bukkit.util.Vector
+import java.util.*
+import kotlin.math.cos
+import kotlin.math.sin
+
+class MasterSmoker : LCDBoss() {
+    override val entityType: EntityType = EntityType.SKELETON
+    override val nonHeadShotDamageResistance: Double = 5.0
+    override val explosionResistance: Double = 3.0
+    override val bossSkillCoolDown: Int = 10
+    override val bossSkills: List<BossSkill> = listOf(MultiShot(), AirStrikeShot())
+
+    override fun spawnEnemy(location: Location) {
+        super.spawnEnemy(location)
+        livingEntity.getTargetEntity(100)
+        manager.matchPlayer.forEach {
+            it.player.playSound(it.player.location, Sound.ENTITY_WITHER_SPAWN, 1f, 1f)
+        }
+    }
+
+    override fun bossNormalAttackRunnable() {
+        val random = Random()
+        object : BukkitRunnable() {
+            override fun run() {
+                if (livingEntity.isDead) {
+                    cancel()
+                    return
+                }
+                livingEntity.world.playSound(livingEntity.location, Sound.ITEM_FIRECHARGE_USE, 3f, 1f)
+                var arrow = 10
+                while (arrow >= 0) {
+                    val attack = livingEntity.launchProjectile(Arrow::class.java)
+                    attack.shooter = livingEntity
+                    attack.damage = getPower()
+                    attack.velocity = attack.velocity.add(Vector(random.nextDouble(), 0.0, random.nextDouble()))
+                    arrow--
+                }
+            }
+        }.runTaskTimer(LeftCrafterDead.instance, 0, 30)
+    }
+
+    override fun setLivingEntitySettings(livingEntity: LivingEntity) {
+        super.setLivingEntitySettings(livingEntity)
+        livingEntity.equipment?.chestplate = ItemStack(Material.DIAMOND_CHESTPLATE)
+        livingEntity.equipment?.leggings = ItemStack(Material.DIAMOND_LEGGINGS)
+        livingEntity.equipment?.boots = ItemStack(Material.DIAMOND_BOOTS)
+        livingEntity.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE)!!.baseValue = 3.0
+    }
+
+    override fun getHealth(): Double {
+        val baseHealth = 500.0
+        val addHealth = when (manager.campaign.determiningDifficulty()) {
+            Campaign.Difficulty.ADVANCED -> 100.0
+            Campaign.Difficulty.EXPERT -> 150.0
+            else -> 20.0
+        }
+        return baseHealth + (addHealth * manager.numberOfSurvivors())
+    }
+
+    override fun getPower(): Double {
+        val basePower = 2.0
+        val addPower = when (manager.campaign.determiningDifficulty()) {
+            Campaign.Difficulty.ADVANCED -> 2.0
+            Campaign.Difficulty.EXPERT -> 2.5
+            else -> 0.0
+        }
+        return basePower + addPower
+    }
+
+    inner class MultiShot : BossSkill() {
+        override fun activationSkill(livingEntity: LivingEntity) {
+            object : BukkitRunnable() {
+                val random = Random()
+                var arrow = 100
+                override fun run() {
+                    if (arrow <= 0 || livingEntity.isDead) {
+                        cancel()
+                        return
+                    }
+                    livingEntity.world.playSound(livingEntity.location, Sound.ENTITY_ZOMBIE_ATTACK_WOODEN_DOOR, 1f, 1f)
+                    val attack = livingEntity.launchProjectile(Arrow::class.java)
+                    attack.velocity = attack.velocity.add(Vector(random.nextDouble(), 0.0, random.nextDouble()))
+                    arrow--
+                }
+            }.runTaskTimer(LeftCrafterDead.instance, 0, 1)
+        }
+    }
+
+    inner class AirStrikeShot : BossSkill() {
+        override fun activationSkill(livingEntity: LivingEntity) {
+            val location = livingEntity.location.clone().add(0.0, 2.0, 0.0)
+            location.world.playSound(location, Sound.ENTITY_LIGHTNING_BOLT_THUNDER, 5f, 1f)
+            location.world.playSound(location, Sound.ENTITY_GENERIC_EXPLODE, 3f, 1f)
+            object : BukkitRunnable() {
+                var count = 10
+                var angle = 0.0
+                override fun run() {
+                    while (angle <= 360) {
+                        val vector = Vector(
+                            sin(Math.toRadians(angle)) / 50,
+                            1.0,
+                            cos(Math.toRadians(angle)) / 50
+                        ).normalize()
+                        val arrow = livingEntity.world.spawnArrow(location, vector, 2f, 12f)
+                        MetadataUtil.setProjectileMetadata(arrow, MetadataUtil.EXPLODE_ARROW)
+                        arrow.shooter = livingEntity
+                        arrow.fireTicks = 100
+                        angle += 10.0
+                    }
+                    count--
+                    if (count <= 0 || livingEntity.isDead) {
+                        cancel()
+                        return
+                    }
+                }
+            }.runTaskTimer(LeftCrafterDead.instance, 0, 5)
+        }
+    }
+}
