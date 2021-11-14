3ac558b1704ebba7278efe903476bb3019c49adc
FratikaK <kbc20c17@stu.kawahara.ac.jp>
2021-11-14 23:17:34 +0900

    Added onPVP,onArrowHit.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/DamageControlListener.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/listener/DamageControlListener.kt
@@ -2,16 +2,20 @@ package com.github.kamunyan.leftcrafterdead.listener
 
 import com.github.kamunyan.leftcrafterdead.LeftCrafterDead
 import com.github.kamunyan.leftcrafterdead.MatchManager
+import com.github.kamunyan.leftcrafterdead.util.MetadataUtil
 import com.github.kamunyan.leftcrafterdead.weapons.GunCategory
 import com.shampaggon.crackshot.events.WeaponDamageEntityEvent
 import com.shampaggon.crackshot.events.WeaponExplodeEvent
 import org.bukkit.Sound
+import org.bukkit.entity.EntityType
 import org.bukkit.entity.HumanEntity
+import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.EntityDeathEvent
+import org.bukkit.event.entity.ProjectileHitEvent
 
 class DamageControlListener : Listener {
     private val plugin = LeftCrafterDead.instance
@@ -29,15 +33,15 @@ class DamageControlListener : Listener {
         lcdPlayer.perk.getGrenade().explosionEffects(location)
         //ダメージを与える処理
         entities.forEach { livingEntity ->
+            lcdPlayer.perk.getGrenade().specialEffects(e.player, livingEntity)
             if (manager.enemyHashMap.containsKey(livingEntity.uniqueId)) {
                 val enemy = manager.enemyHashMap[livingEntity.uniqueId]!!
-                lcdPlayer.perk.getGrenade().specialEffects(e.player, livingEntity)
                 if (livingEntity is HumanEntity || livingEntity.isDead) {
                     return@forEach
                 }
                 val distance = (livingEntity.location.distance(location)).toInt()
                 var lastDamage = weaponDamage - (distance * distanceDecay) - enemy.explosionResistance
-                if (lastDamage <= 0){
+                if (lastDamage <= 0) {
                     lastDamage = 0.0
                 }
                 livingEntity.damage(lastDamage, e.player)
@@ -51,7 +55,9 @@ class DamageControlListener : Listener {
             e.damage = 0.0
             return
         }
-        if (e.victim is Player) {
+        if (e.victim.type == EntityType.PLAYER) {
+            println("キャンセル！")
+            e.isCancelled = true
             return
         }
         if (!manager.enemyHashMap.containsKey(e.victim.uniqueId)) {
@@ -80,7 +86,7 @@ class DamageControlListener : Listener {
 
         val uuid = e.entity.uniqueId
         if (manager.enemyHashMap.containsKey(uuid)) {
-            manager.enemyHashMap[uuid]?.enemyDeathEffects()
+            manager.enemyHashMap[uuid]?.enemyDeathEffects(e.entity)
             manager.enemyHashMap.remove(uuid)
         }
     }
@@ -88,14 +94,28 @@ class DamageControlListener : Listener {
     @EventHandler
     fun onPlayerDamage(e: EntityDamageByEntityEvent) {
         val player = e.entity
-        if (player !is Player) {
+        if (player.type != EntityType.PLAYER) {
             return
         }
         if (!manager.enemyHashMap.containsKey(e.damager.uniqueId)) {
             return
         }
         val enemy = manager.enemyHashMap[e.damager.uniqueId]!!
+        enemy.attackSpecialEffects(e.damager as LivingEntity, player as LivingEntity)
         e.damage = enemy.getPower()
-        enemy.attackSpecialEffects(player)
+    }
+
+    @EventHandler
+    fun onPvP(e: EntityDamageByEntityEvent) {
+        if (e.entity.type == e.damager.type) {
+            e.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun onArrowHit(e: ProjectileHitEvent) {
+        if (e.entity.hasMetadata(MetadataUtil.EXPLODE_ARROW)) {
+            e.entity.location.createExplosion(2.0f, false, false)
+        }
     }
 }
