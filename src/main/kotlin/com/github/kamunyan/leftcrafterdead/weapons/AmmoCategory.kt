4d33500e46504baa61970a9dfabdbe76334b4aac
FratikaK <kamunyan17@gmail.com>
2022-02-01 13:28:55 +0900

    Changed item slot.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/AmmoCategory.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/AmmoCategory.kt
@@ -6,30 +6,30 @@ enum class AmmoCategory {
     RIFLE {
         override val material: Material = Material.STICK
         override val ammoCustomName: String = "ライフルの弾"
-        override val itemSlot: ArrayList<Int> = arrayListOf(9,10,11,12,13,14,15,16,17)
+        override val itemSlot = 9
     },
     SUB {
         override val material: Material = Material.IRON_NUGGET
         override val ammoCustomName: String = "ハンドガンの弾"
-        override val itemSlot: ArrayList<Int> = arrayListOf(18,19,20,21,22,23,24,25,26)
+        override val itemSlot = 10
     },
     SHELL {
         override val material: Material = Material.GOLD_NUGGET
         override val ammoCustomName: String = "ショットガンの弾"
-        override val itemSlot: ArrayList<Int> = arrayListOf(27,28,29,30,31,32)
+        override val itemSlot = 11
     },
     MAGNUM {
         override val material: Material = Material.BLAZE_ROD
         override val ammoCustomName: String = "マグナムの弾"
-        override val itemSlot: ArrayList<Int> = arrayListOf(33,34,35)
+        override val itemSlot = 12
     },
     UNKNOWN {
         override val material: Material = Material.AIR
         override val ammoCustomName: String = ""
-        override val itemSlot: ArrayList<Int> = arrayListOf()
+        override val itemSlot = 35
     };
 
     abstract val material: Material
     abstract val ammoCustomName: String
-    abstract val itemSlot: ArrayList<Int>
+    abstract val itemSlot: Int
 }
