7a1ff39ceb1a4a3d544689f2d2e870be868ae80f
FratikaK <kamunyan17@gmail.com>
2022-02-10 14:41:45 +0900

    Added handling of Trader.

--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/util/Trader.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/util/Trader.kt
@@ -1,14 +1,18 @@
 package com.github.kamunyan.leftcrafterdead.util
 
+import com.github.kamunyan.leftcrafterdead.MatchManager
+import com.github.kamunyan.leftcrafterdead.weapons.AmmoCategory
 import com.github.kamunyan.leftcrafterdead.weapons.GunCategory
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.entity.Player
+import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 
 object Trader {
+    private val manager = MatchManager
     val weaponShopList = linkedMapOf(
         GunCategory.ASSAULT_RIFLE to LinkedHashMap<String, Int>(),
         GunCategory.SUB_MACHINE_GUN to LinkedHashMap(),
@@ -24,9 +28,11 @@ object Trader {
     //non item
     private val N = Pair(u.generateMetaItem(Material.BLACK_STAINED_GLASS_PANE, ""), fun(_: Player) {})
 
+    //back item
+    private val backItem = u.generateMetaItem(Material.OAK_DOOR, "${ChatColor.RED}戻る")
+
     private val shopDisplay = Bukkit.createInventory(null, 9, Component.text("Select Category"))
     private val weaponDisplay = Bukkit.createInventory(null, 54, Component.text("Weapons"))
-    private val subGadgetDisplay = Bukkit.createInventory(null, 9, Component.text("Sub Gadget"))
     private val ammoDisplay = Bukkit.createInventory(null, 9, Component.text("弾薬補給"))
 
     val shopDisplayFuncs = HashMap<Int, Pair<ItemStack, (player: Player) -> Unit>>()
@@ -34,24 +40,22 @@ object Trader {
     fun createTraderDisplay() {
         createShopDisplay()
         createWeaponDisplay()
-        createSubGadgetDisplay()
         createAmmoDisplay()
     }
 
-    fun spawnTrader(){}
+    fun spawnTrader() {}
 
-    fun deleteTrader(){}
+    fun deleteTrader() {}
 
     private fun createShopDisplay() {
         val weapon = u.generateMetaItem(Material.NETHERITE_AXE, "${ChatColor.AQUA}武器を購入する")
         val gadget = u.generateMetaItem(Material.TNT_MINECART, "${ChatColor.GREEN}サブガジェットを購入する")
         val ammo = u.generateMetaItem(Material.GOLD_NUGGET, "${ChatColor.YELLOW}弾薬補給")
         val weaponPair = Pair(weapon, fun(p: Player) { p.openInventory(weaponDisplay) })
-        val gadgetPair = Pair(gadget, fun(p: Player) { p.openInventory(subGadgetDisplay) })
         val ammoPair = Pair(ammo, fun(p: Player) { p.openInventory(ammoDisplay) })
-        val itemArray = arrayOf(N, N, N, weaponPair, gadgetPair, ammoPair, N, N, N)
-        for (i in (0..8)){
-            shopDisplay.setItem(i,itemArray[i].first)
+        val itemArray = arrayOf(N, N, N, weaponPair, N, ammoPair, N, N, N)
+        for (i in (0..8)) {
+            shopDisplay.setItem(i, itemArray[i].first)
             shopDisplayFuncs[i] = itemArray[i]
         }
     }
@@ -60,8 +64,7 @@ object Trader {
 
     }
 
-    private fun createSubGadgetDisplay() {}
-
-    private fun createAmmoDisplay() {}
+    private fun createAmmoDisplay() {
+    }
 
 }
--- src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/GunCategory.kt
+++ src/main/kotlin/com/github/kamunyan/leftcrafterdead/weapons/GunCategory.kt
@@ -2,62 +2,62 @@ package com.github.kamunyan.leftcrafterdead.weapons
 
 enum class GunCategory {
     ASSAULT_RIFLE {
-        override fun getWeaponList(): List<String> {
-            return listOf("AK12","Mini21")
+        override fun getWeaponList(): ArrayList<String> {
+            return arrayListOf("AK12","Mini21")
         }
 
         override val ammoType: AmmoCategory
             get() = AmmoCategory.RIFLE
     },
     SUB_MACHINE_GUN {
-        override fun getWeaponList(): List<String> {
-            return listOf("MAC10")
+        override fun getWeaponList(): ArrayList<String> {
+            return arrayListOf("MAC10")
         }
 
         override val ammoType: AmmoCategory
             get() = AmmoCategory.SUB
     },
     SHOTGUN {
-        override fun getWeaponList(): List<String> {
-            return listOf("M31")
+        override fun getWeaponList(): ArrayList<String> {
+            return arrayListOf("M31")
         }
 
         override val ammoType: AmmoCategory
             get() = AmmoCategory.SHELL
     },
     HANDGUN {
-        override fun getWeaponList(): List<String> {
-            return listOf("P226")
+        override fun getWeaponList(): ArrayList<String> {
+            return arrayListOf("P226")
         }
 
         override val ammoType: AmmoCategory
             get() = AmmoCategory.SUB
     },
     AKIMBO{
-        override fun getWeaponList(): List<String> {
-            return listOf()
+        override fun getWeaponList(): ArrayList<String> {
+            return arrayListOf()
         }
 
         override val ammoType: AmmoCategory
             get() = AmmoCategory.SUB
     },
     GRENADE {
-        override fun getWeaponList(): List<String> {
-            return listOf("Grenade","Heal Grenade","Concussion","Cluster Bomb")
+        override fun getWeaponList(): ArrayList<String> {
+            return arrayListOf("Grenade","Heal Grenade","Concussion","Cluster Bomb")
         }
 
         override val ammoType: AmmoCategory
             get() = AmmoCategory.UNKNOWN
     },
     UNKNOWN{
-        override fun getWeaponList(): List<String> {
-            return listOf()
+        override fun getWeaponList(): ArrayList<String> {
+            return arrayListOf()
         }
 
         override val ammoType: AmmoCategory
             get() = AmmoCategory.UNKNOWN
     };
 
-    abstract fun getWeaponList(): List<String>
+    abstract fun getWeaponList(): ArrayList<String>
     abstract val ammoType:AmmoCategory
 }
